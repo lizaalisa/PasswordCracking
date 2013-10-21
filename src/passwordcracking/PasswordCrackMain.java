5a159f44127f91a44477318f66d5cb18c99d2ce5
Ichiorochi <rudiodder@live.dk>
2013-10-21 13:08:38 +0200

    Attempted merge and thread pooling

--- src/passwordcracking/PasswordCrackMain.java
+++ src/passwordcracking/PasswordCrackMain.java
@@ -3,6 +3,9 @@
  * and open the template in the editor.
  */
 package passwordcracking;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 /**
  *
@@ -15,5 +18,14 @@ public class PasswordCrackMain {
      */
     public static void main(String[] args) {
         //we create and run threads here
+        Runnable master = new Master();
+        Runnable slave1 = new Slave();
+        Runnable slave2 = new Slave();
+        
+        ExecutorService pool = Executors.newCachedThreadPool();
+        pool.execute(master);
+        pool.execute(slave1);
+        pool.execute(slave2);
+        pool.shutdown();
     }
 }
