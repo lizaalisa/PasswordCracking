0e0f04ee70a662faae4a85ccf747429c301b09f4
Ichiorochi <rudiodder@live.dk>
2013-10-21 13:23:14 +0200

    Attempted merge and thread pooling

--- src/passwordcracking/Master.java
+++ src/passwordcracking/Master.java
@@ -4,15 +4,27 @@
  */
 package passwordcracking;
 
+import java.io.BufferedReader;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
+import java.util.List;
+import sun.misc.BASE64Encoder;
+
 /**
  *
  * @author Hina
  */
-public class Master implements Runnable{
+public class Master implements Runnable {
+
+    public static final String MESSAGE_DIGEST_ALGORITHM = "SHA";
 
     @Override
     public void run() {
-        
-    }
+
     
+}
 }
