7e89bd69db722e2e8481c2c48b981783ee2a9879
Ichiorochi <rudiodder@live.dk>
2013-10-24 10:19:09 +0200

    Trying

--- src/passwordcracking/Slave.java
+++ src/passwordcracking/Slave.java
@@ -26,13 +26,15 @@ public class Slave implements Runnable {
     private static MessageDigest messageDigest;
     public static final String MESSAGE_DIGEST_ALGORITHM = "SHA";
     private static final Logger LOGGER = Logger.getLogger("passwordCracker");
-    private final String username;
-    private final String password;
-    private String str;
+//    private final String username;
+//    private final String password;
+    private String word;
     
 
     @Override
     public void run() {
+        
+        //take() = ;
         
 }
 }
